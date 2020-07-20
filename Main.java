1e342d949850f52d3522a5886c3f1c597ac98cef
Agnehska <45421349+Agnehska@users.noreply.github.com>
2020-07-20 15:25:56 +0500

    Add files via upload
    Задание №1 по Java FX

--- Main.java
+++ Main.java
@@ -6,15 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("UpRead.Ru");
-        primaryStage.setScene(new Scene(root, 400, 400));
+        primaryStage.setTitle("Создание простейшей визуальнойпрограммы на Java");
+        primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.show();
+
+
     }
+
 
     public static void main(String[] args) {
         launch(args);
