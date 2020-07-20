7406acfacd4566aa777224a72b4aa958f2001454
Agnehska <45421349+Agnehska@users.noreply.github.com>
2020-07-20 15:16:14 +0500

    Add files via upload
    Тренировочное задание №2

--- Main.java
+++ Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Test");
-        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.setTitle("test.fxml");
+        primaryStage.setScene(new Scene(root, 600, 100));
         primaryStage.show();
     }
 
